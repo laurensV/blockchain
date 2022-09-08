8d85277d9d217962ad3b5bf9a9217c68b5dba766
Laurens Verspeek <l.verspeek@gmail.com>
2022-09-08 11:34:18 +0200

    add hash to Block

--- src/block.js
+++ src/block.js
@@ -1,8 +1,15 @@
+const crypto = require("crypto");
+const SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
+
 class Block {
     constructor(data = [], timestamp = Math.floor(Date.now() / 1000)) {
         this.timestamp = timestamp;
-        // this.data should contain information like transactions.
         this.data = data;
+        this.hash = this.getHash();
+        this.prevHash = "";
+    }
+    getHash() {
+        return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data));
     }
 }
 
